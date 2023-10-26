fa23ab3e67c0fa4b77cc144ff0f43e7e5da5762a
vincent <tranhao491999@gmail.com>
2023-10-26 10:44:28 +0700

    c

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,6 +1,8 @@
 import { loginApi } from "apis/auth";
 import "./style.scss";
 
+import ImageMain from "assets/images/login/img-main.png";
+
 function Login() {
   // console.log(import.meta.env.VITE_TEST);
   // console.log(import.meta.env.VITE_TEST1);
@@ -14,8 +16,21 @@ function Login() {
 
   return (
     <div className="max-w-[1170px] mx-auto px-[16px] pt-[100px] pb-[225px]">
-      <div onClick={onSubmit}>login1</div>
-      <div className="text-3xl">test css</div>
+      {/* <div onClick={onSubmit}>login1</div>
+      <div className="text-3xl font-sp600">test css</div> */}
+      <div className="flex">
+        <div>
+          <img src={ImageMain} alt="img" />
+        </div>
+        <div className="px-[100px] pt-[70px] pb-[100px] flex-1">
+          <div className="text-[#0d0d0d] mb-[10px] text-[40px] font-sp400 leading-[1.35]">
+            LOGIN
+          </div>
+          <div>
+            아트앤가이드의 다양한 서비스를 이용하시려면 로그인을 해주세요
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
